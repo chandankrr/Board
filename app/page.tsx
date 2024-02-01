ea3a23c14c6fd80a2cf5990e0af97219aa6cc2f9
Chandan Kumar <chandankrr.91@gmail.com>
2024-02-01 17:32:42 +0530

    Add UserButton
    Show UserButton for authenticated user to manage their account

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
-  return <p>Hello Board!</p>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated user only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
