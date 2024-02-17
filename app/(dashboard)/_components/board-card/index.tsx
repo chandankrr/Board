f000436ef3e84738c9e588b346aa77f0249eb46c
Chandan Kumar <chandankrr.91@gmail.com>
2024-02-17 12:06:20 +0530

    Added dropdown menu to copy board link

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -2,11 +2,14 @@
 
 import { useAuth } from '@clerk/nextjs';
 import { formatDistanceToNow } from 'date-fns';
+import { MoreHorizontal } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Actions } from '@/components/actions';
 import { Skeleton } from '@/components/ui/skeleton';
 
+import { Button } from '@/components/ui/button';
 import { Footer } from './footer';
 import { Overlay } from './overlay';
 
@@ -42,6 +45,11 @@ export const BoardCard = ({
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
+          <Actions id={id} title={title} side="right">
+            <Button className="absolute right-1 top-1 opacity-0 group-hover:opacity-100 bg-transparent group-hover:bg-transparent transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
+            </Button>
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
